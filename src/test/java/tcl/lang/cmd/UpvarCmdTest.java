3f7f37f671a4691dcf05d0b6b5c485c327864188
Tom Poindexter <tpoindex@gmail.com>
2010-06-14 09:41:25 -0600

    note expected test failures, due to differences in order of elements returned by "array names"

--- src/test/java/tcl/lang/cmd/UpvarCmdTest.java
+++ src/test/java/tcl/lang/cmd/UpvarCmdTest.java
@@ -1,11 +1,19 @@
 
 package tcl.lang.cmd;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import tcl.lang.TclCmdTest;
 
 public class UpvarCmdTest extends TclCmdTest {
 	public void testCmd() throws Exception {
+		LinkedList expectedFailureList = new LinkedList(Arrays.asList( new String[] {
+				// these test pass, except order of list returned by "array names" if different
+				"upvar-3.5", "upvar-3.6"
+			}));
+		
 		String resName = "/tcl/lang/cmd/upvar.test";
-		tclTestResource(resName);
+		tclTestResource(resName, expectedFailureList);
 	}
 }
