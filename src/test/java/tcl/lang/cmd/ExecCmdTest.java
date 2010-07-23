db55f1d9196f2a2125c5193bdcf0bb86cb2a6ce7
Dan Bodoh <dan.bodoh@gmail.com>
2010-07-22 22:02:51 -0500

    Use testConstraint in exec.test for exec-1.3 and exec-6.3 , instead of ExecCmdTest.java, because they fail intermittently (due to stdin byte stealing)

--- src/test/java/tcl/lang/cmd/ExecCmdTest.java
+++ src/test/java/tcl/lang/cmd/ExecCmdTest.java
@@ -11,9 +11,12 @@ public class ExecCmdTest extends TclCmdTest {
 				// exec-1.3 and exec-63 fail because the JVM process model doesn't allow for proper STDIN 
 				// file descriptor inheritance, instead using a pipe model.  Too much of stdin can get
 				// eaten up because of this.  The solution is to create platform-specific subclasses
-				// of TclProcess which do proper inheritance.
-				"exec-1.3",
-				"exec-6.3",
+				// of TclProcess which do proper inheritance.  However, these two test fail only
+				// occassionally, so we inserted the "stdinInherit" testconstraint into exec.test
+				// which should be removed when we have platform-specific exec.
+				//"exec-1.3",
+				//"exec-6.3",
+				
 				// exec-11.5 and exec-17.1 fail because JVM doesn't allow background processes to survive the JVM
 				// if they read from STDIN or write to STDOUT/STDERR.  There's no direct access
 				// in the JVM to filedescriptors 0, 1, 2; we're only given InputStream() and OutputStream()
