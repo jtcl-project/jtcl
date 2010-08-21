ed959d9a0e3848b62f2d391e3f8e6413d0aab57a
Dan Bodoh <dan.bodoh@gmail.com>
2010-08-21 10:18:48 -0500

    Set expectedFailureList in ErrorCmdTest.java; remove unused expectedFailureList from EvalCmdTest.java

--- src/test/java/tcl/lang/cmd/ErrorCmdTest.java
+++ src/test/java/tcl/lang/cmd/ErrorCmdTest.java
@@ -1,10 +1,17 @@
 package tcl.lang.cmd;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import tcl.lang.TclCmdTest;
 
 public class ErrorCmdTest  extends TclCmdTest {
-	public void testCmd() throws Exception {
+	public void testCmd() throws Exception {	LinkedList<String> expectedFailureList = new LinkedList<String>(Arrays.asList( new String[] {
+			// difference in 'invoked from within' and 'while executing'
+            "error-1.3",
+            "error-2.6" 
+        }));
 		String resName = "/tcl/lang/cmd/error.test";
-		tclTestResource(resName);
+		tclTestResource(resName, expectedFailureList);
 	}
 }
