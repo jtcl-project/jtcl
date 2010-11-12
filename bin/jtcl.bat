fa0b64df4bc8f0f863ff9610e4223a04b82b721f
Tom Poindexter <tpoindex@gmail.com>
2010-11-11 23:09:56 -0700

    getting started page now references jtcl shell and batch files

--- bin/jtcl.bat
+++ bin/jtcl.bat
@@ -7,7 +7,7 @@ rem
 rem JAVA_HOME  - directory of JDK/JRE, if not set then 'java' must be found on PATH
 rem CLASSPATH  - colon separated list of additional jar files & class directories
 rem JAVA_OPTS  - list of JVM options, e.g. "-Xmx256m -Dfoo=bar"
-rem TCLLIBPATH - space separated list of Tcl libraries directories
+rem TCLLIBPATH - space separated list of Tcl library directories
 rem
 
 
