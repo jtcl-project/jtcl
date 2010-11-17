1bf78d5609b6d3a06be85f30a6efb58d9ea213c2
Tom Poindexter <tpoindex@gmail.com>
2010-11-16 21:16:44 -0700

    changed release to unpack into a directory, doc updates

--- misc/mkRelease.sh
+++ misc/mkRelease.sh
@@ -3,14 +3,15 @@
 version=2.0.0-a1
 
 dir=/tmp/jtcl-$$.d
-mkdir $dir
+mkdir -p $dir/jtcl-$version
 
-cp ../README             $dir
-cp ../bin/jtcl*          $dir
-cp ../target/jtcl-*.jar  $dir
-cp ../licenses/*         $dir
+cp ../README             $dir/jtcl-$version
+cp ../bin/*              $dir/jtcl-$version
+cp ../target/jtcl-*.jar  $dir/jtcl-$version
+cp ../licenses/*         $dir/jtcl-$version
 
 cd $dir
-zip ../jtcl-${version}.zip *
+zip -r jtcl-${version}.zip jtcl-$version/*
+mv jtcl-${version}.zip /tmp
 cd
 rm -rf $dir
