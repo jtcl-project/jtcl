06f1cb0d2aeff93384887fc2d5878c937e1640e2
Tom Poindexter <tpoindex@gmail.com>
2012-01-07 19:54:02 -0700

    lassign test and driver

---
+++ src/test/java/tcl/lang/cmd/LassignCmdTest.java
@@ -0,0 +1,10 @@
+package tcl.lang.cmd;
+
+import tcl.lang.TclCmdTest;
+
+public class LassignCmdTest extends TclCmdTest {
+	public void testCmd() throws Exception {
+		String resName = "/tcl/lang/cmd/lassign.test";
+		tclTestResource(resName);
+	}
+}
