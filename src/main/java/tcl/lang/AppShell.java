31aadca6acb8dd4bbd0c5d8681e6639ad8e26ee4
Tom Poindexter <tpoindex@gmail.com>
2012-02-20 21:46:04 -0700

    AppShell didn't find the correct MANIFEST.MF resource for Java 1.5.

--- src/main/java/tcl/lang/AppShell.java
+++ src/main/java/tcl/lang/AppShell.java
@@ -16,11 +16,13 @@ package tcl.lang;
 
 import java.io.InputStream;
 import java.util.jar.Manifest;
-
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
- * The Shell class is similar to the Tclsh program: you can use it to execute a
- * Tcl script or enter Tcl command interactively at the command prompt.
+ * The AppShell class expects to load and execute a Tcl resource file by
+ * retrieving the JTcl-Main attribute from the manifest. See paraffin.tcl to
+ * build a conforming JAR file.
  */
 
 public class AppShell {
@@ -29,14 +31,15 @@ public class AppShell {
 	 * Set to true to force tcl_interactive to 0 in the shell
 	 */
 	public static boolean forceNonInteractive = false;
-	
+
 	/**
-	 * The attribute in META-INF/MANIFEST.MF denoting the resource file to execute.
+	 * The attribute in META-INF/MANIFEST.MF denoting the resource file to
+	 * execute.
 	 */
 	private static String JTCL_MAIN = "JTcl-Main";
-	
+
 	/**
-	 * Main program for tclsh and most other Tcl-based applications.
+	 * Main program for AppShell.
 	 * 
 	 * Results: None.
 	 * 
@@ -49,18 +52,49 @@ public class AppShell {
 	public static void main(String args[]) {
 		Manifest mf;
 		try {
-			InputStream inputStream = AppShell.class.getClassLoader().getResourceAsStream("META-INF/MANIFEST.MF");
+			InputStream inputStream = AppShell.class.getProtectionDomain().getClassLoader()
+					.getResourceAsStream("META-INF/MANIFEST.MF");
 			mf = new Manifest(inputStream);
 			inputStream.close();
 		} catch (Exception e) {
 			throw new TclRuntimeError("META-INF/MANIFEST.MF does not exist or not running from a .jar file: " + e);
 		}
-		String fileName = mf.getMainAttributes().getValue(JTCL_MAIN);
+
+		String fileName = mf.getMainAttributes().getValue(JTCL_MAIN); // only in
+																		// 1.6+
+
+		if (fileName == null) {
+			// java 1.5 - have to read the app jar file directly
+			ZipInputStream zipin = null;
+			try {
+				InputStream inputStream = AppShell.class.getProtectionDomain().getCodeSource().getLocation()
+						.openConnection().getInputStream();
+				zipin = new ZipInputStream(inputStream);
+				ZipEntry entry = zipin.getNextEntry();
+				while (entry != null) {
+					if ("META-INF/MANIFEST.MF".equals(entry.getName())) {
+						mf = new Manifest(zipin);
+						break;
+					}
+					entry = zipin.getNextEntry();
+				}
+			} catch (Exception e) {
+				// ignore
+			} finally {
+				try {
+					zipin.close();
+				} catch (Exception e) {
+					// ignore
+				}
+			}
+			fileName = mf.getMainAttributes().getValue(JTCL_MAIN);
+		}
+
 		if (fileName == null) {
 			throw new TclRuntimeError("META-INF/MANIFEST.MF does not contain \"JTcl-Main\" attribute");
 		} else {
 			while (fileName.startsWith("/")) {
-			    fileName = fileName.substring(1);
+				fileName = fileName.substring(1);
 			}
 			while (fileName.endsWith("/")) {
 				fileName = fileName.substring(0, fileName.length() - 1);
@@ -86,28 +120,26 @@ public class AppShell {
 			}
 			interp.setVar("argv", argv, TCL.GLOBAL_ONLY);
 			interp.setVar("argc", java.lang.Integer.toString(argc), TCL.GLOBAL_ONLY);
-		
+
 			int lastSlash = fileName.lastIndexOf('/');
 			String dir = "resource:/" + (lastSlash >= 0 ? fileName.substring(0, lastSlash) : "");
 			TclObject auto_path = null;
 			try {
 				auto_path = interp.getVar("auto_path", TCL.GLOBAL_ONLY);
 			} catch (TclException e) {
-				//ignore
+				// ignore
 			}
 			if (auto_path == null) {
 				interp.setVar("auto_path", TclString.newInstance(dir), TCL.GLOBAL_ONLY);
 			} else {
 				TclList.append(interp, auto_path, TclString.newInstance(dir));
 			}
-		
+
 		} catch (TclException e) {
 			throw new TclRuntimeError("unexpected TclException: " + e);
 		} finally {
 			argv.release();
 		}
-
-
 
 		int exitCode = 0;
 		try {
@@ -117,16 +149,15 @@ public class AppShell {
 			if (code == TCL.RETURN) {
 				code = interp.updateReturnInfo();
 				if (code != TCL.OK) {
-					System.err
-							.println("command returned bad code: " + code);
+					System.err.println("command returned bad code: " + code);
 					exitCode = 2;
 				}
 			} else if (code == TCL.ERROR) {
 				try {
-					TclObject errorInfo = interp.getVar("errorInfo",TCL.GLOBAL_ONLY);
+					TclObject errorInfo = interp.getVar("errorInfo", TCL.GLOBAL_ONLY);
 					System.err.println(errorInfo.toString());
 				} catch (TclException e1) {
-					System.err.println(interp.getResult().toString());				
+					System.err.println(interp.getResult().toString());
 				}
 				exitCode = 1;
 			} else {
@@ -149,7 +180,6 @@ public class AppShell {
 
 		interp.dispose();
 		System.exit(exitCode);
-		
 
 	}
-} 
+}
