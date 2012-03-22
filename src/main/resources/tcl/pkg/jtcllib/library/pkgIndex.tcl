452a0f8fafcc5f69cb7e9f9f638ef2804da9624c
Bruce Johnson <bruce@onemoonscientific.com>
2012-03-22 15:57:36 -0400

    Add fleet to jtcllib pkgIndex

--- src/main/resources/tcl/pkg/jtcllib/library/pkgIndex.tcl
+++ src/main/resources/tcl/pkg/jtcllib/library/pkgIndex.tcl
@@ -12,5 +12,6 @@ if {[lsearch -exact $::auto_path $dir] == -1} {
 set maindir $dir
 set dir [file join $maindir hyde] ;	 source [file join $dir pkgIndex.tcl]
 set dir [file join $maindir ziplib] ;	 source [file join $dir pkgIndex.tcl]
+set dir [file join $maindir fleet] ;	 source [file join $dir pkgIndex.tcl]
 unset maindir
 
