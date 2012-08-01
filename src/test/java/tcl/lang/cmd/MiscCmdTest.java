3205edcc7f2b15951236915643338c74b53587a8
Tom Poindexter <tpoindex@gmail.com>
2012-07-31 21:43:09 -0600

    previously failing test ("io-14.9") now seems to be working in Java 1.6.0_31

--- src/test/java/tcl/lang/cmd/MiscCmdTest.java
+++ src/test/java/tcl/lang/cmd/MiscCmdTest.java
@@ -18,7 +18,8 @@ public class MiscCmdTest extends TclCmdTest {
 	
 	public void testIo() throws Exception {
 		LinkedList<String> expectedFailureList = new LinkedList<String>(Arrays.asList( new String[] {
-				"io-14.9" // test requires that we can exec a new tcltest environment
+				//  previously failing test now seems to be working in Java 1.6.0_31
+				//  "io-14.9" // test requires that we can exec a new tcltest environment
 			}));
 		String resName = "/tcl/lang/cmd/io.test";
 		tclTestResource(TCLTEST_NAMEOFEXECUTABLE,resName, expectedFailureList);
